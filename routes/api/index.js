a457b45458bb14218a388793f08456828820ec67
ijeli <legaspi.jel@gmail.com>
2021-02-10 22:08:04 -0500

    added image_repo directory as well as routes for GET and POST

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const locationRoutes = require('./locations');
+const imageFileRoutes = require('./imageFileRoutes');
 const db = require('../../models');
+
+router.use('/img', imageFileRoutes);
 
 module.exports = router;
