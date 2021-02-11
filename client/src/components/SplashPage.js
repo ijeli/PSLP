a7bb329ff3470fe8da895f05e2ba32b35728026c
ijeli <legaspi.jel@gmail.com>
2021-02-11 09:36:05 -0500

    added html and logic in the client/react to upload image files

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -4,14 +4,77 @@ import axios from 'axios';
 import exifr from 'exifr'; //newer model which support .heic
 
 const SplashPage = () => {
+    const [file, setFile] = useState();
+    const [lat, setLat] = useState();
+    const [long, setLong] = useState();
+
+    useEffect( () => {
+        API.getAllImageFiles()
+            .then(res => console.log(res.data))
+            .catch(console.err);
+    }, [])
+
+    function metaDataExportObject(LatDirect, LongDirect, Latitude, Longitude) {
+        this.LatDirect = LatDirect;
+        this.LongDirect = LongDirect;
+        this.Latitude = Latitude;
+        this.Longitude = Longitude;
+    }
+
+    const getExifr = async () => {
+        let metaDataExport;
+
+        let output = await exifr.parse(file)
+        console.log(output);
+        if(!output) {
+            metaDataExport = new metaDataExportObject("N/A","N/A",0,0)
+        } else {
+            metaDataExport = (!output.GPSLatitudeRef) ? new metaDataExportObject("N/A","N/A",0,0) :
+                new metaDataExportObject(
+                    output.GPSLatitudeRef,
+                    output.GPSLongitudeRef,
+                    output.latitude,
+                    output.longitude
+                )
+        }
+        return (metaDataExport);
+    }
+
+    const send = async event => {
+        const gpsMetaData = await getExifr();
+        const data = new FormData();
+        data.append('file', file);
+        data.append('fileMetaData',  gpsMetaData.LatDirect);
+        data.append('fileMetaData',  gpsMetaData.LongDirect);
+        data.append('fileMetaData',  gpsMetaData.Latitude);
+        data.append('fileMetaData',  gpsMetaData.Longitude);
+
+        let postOptions = {
+            method: 'POST',
+            body: data
+        }
+
+        API.postOneImageFile(postOptions)
+            .then(res => {
+                console.log(res.data);
+                alert('Image Uploaded!');
+            })
+            .catch(console.err);
+    }
 
     return (
         <div style={{width: "550px"}}>
-            <img style={{float: "left"}} src="https://via.placeholder.com/200"></img>
-            <img style={{float: "left"}} src="https://via.placeholder.com/200"></img>
+            <div>
+                <img style={{float: "left"}} src="https://via.placeholder.com/200"></img>
+                <img style={{float: "right"}} src="https://via.placeholder.com/200"></img>
+            </div>
+            <div>
+                <img style={{float: "left"}} src="https://via.placeholder.com/200"></img>
+                <img style={{float: "right"}} src="https://via.placeholder.com/200"></img>
+            </div>
 
-            <img style={{float: "right"}} src="https://via.placeholder.com/200"></img>
-            <img style={{float: "right"}} src="https://via.placeholder.com/200"></img>
+            <input type='file' onChange = { event => setFile(event.target.files[0])}/>
+            <button onClick={send}>Upload</button>
         </div>
     )
 }
--- client/src/utils/API/API.js
+++ client/src/utils/API/API.js
@@ -1,4 +1,6 @@
 export default {
     getAllImageFiles: () => fetch(`http://localhost:3001/api/img`),
-    getOneImageFile: (id) => fetch(`http://localhost:3001/api/img/${id}`)
+    getOneImageFile: (id) => fetch(`http://localhost:3001/api/img/${id}`),
+
+    postOneImageFile: (data) => fetch(`http://localhost:3001/api/img/upload`, data)
 }
